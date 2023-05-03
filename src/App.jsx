10f8e471f021a1a9840aec5871fcc180154af51b
Shivam <shivamv1332@gmail.com>
2023-05-03 23:08:44 +0530

    better

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import './App.css'
-
-const todoList = ['item-1', 'item-2']
+import deletelogo from './assets/delete.png'
+import { useState, useRef } from 'react'
 
 function App() {
+
+    const [todoList, setList] = useState([])
+    const [item, setItem] = useState('')
+    const inputRef = useRef(null);
+
+    const addItem = () => {
+        setList((state) => [...state, item]);
+        setItem('')
+        inputRef.current.focus();
+    }
+
+    const deleteItem = (index) => {
+        setList(todoList.filter((v, i) => {
+            return i != index;
+        }))
+    }
+
+    const handleKeyDown = (event) => {
+        if(event.key === 'Enter') {
+            addItem();
+        }
+    }
     
     return (
         <main className='container'>
             <h2>Items To Buy</h2>
-            <input type="text" name="" id="" />
-            <button>Add</button>
-            <ul>{todoList.map((item, index) => (
-                <li key={item+index}>{item}</li>
+            <div className="inputs">
+            <input autoFocus ref={inputRef} type="text" onKeyDown={handleKeyDown} onChange={(e) => setItem(e.target.value)} value={item}/>
+            <button onClick={addItem}>Add</button>
+            </div>
+            <ul>{todoList.map((list_item, index) => (
+                <li key={list_item+index}>{list_item}<img onClick={()=>{deleteItem(index)}} src={deletelogo} /></li>
             ))}</ul>
         </main>
     )
